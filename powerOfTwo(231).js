2bb0dd47071cc0903d4e6592a24575f65b2e4c4d
Anas <anasanchu3004@gmail.com>
2024-03-22 21:36:18 +0530

    231. Power of Two

---
+++ powerOfTwo(231).js
@@ -0,0 +1,46 @@
+// function Power(n) { 
+
+
+//     let number = []
+//     for(i=0;i<n;i++){
+//         number.push(Math.pow(2,i))
+        
+
+//     }
+
+             // 1 method
+
+//     if(number.includes(n)){
+//         console.log("true");
+
+//     }
+//     else{
+//         console.log("false");
+//     }
+
+
+             //2 method
+
+//   let res=  number.some((data)=>{
+//     return data===n
+//   })
+//   return res
+
+
+
+
+
+function Power(n) { 
+    let i = 1;
+    while (n > i) {
+        i = i * 2;
+        console.log(i);
+    }
+    console.log(n === i);
+}
+
+Power(4)
+
+
+
+
