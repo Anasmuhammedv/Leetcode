dec34f1a7a28393694107a71054296dfc1c6e82d
Anas <anasanchu3004@gmail.com>
2024-03-22 21:37:24 +0530

    1. Two Sum

---
+++ twoSum(1).js
@@ -0,0 +1,26 @@
+function sum(arr,target) {
+    // let arr2 =[]
+
+    // for(let i=0;i<arr.length;i++){
+    //     for(let j=i+1;j<arr.length;j++){
+    //         if (arr[i]+arr[j]===target){
+
+    //             arr2.push(i,j)
+    //         }
+            
+    //     }
+    // }
+    // return arr2
+
+
+
+
+    
+
+
+    
+
+    
+}
+let arr=[11,15,5,4]
+console.log(sum(arr,9));
