828e9a850467f3a804af354eb630fef05e1d7663
Anas <anasanchu3004@gmail.com>
2024-08-27 20:37:48 +0530

    136 single number return code is done

--- singleNumber(136).js
+++ singleNumber(136).js
@@ -1,5 +1,16 @@
-function Unique(arr) {
-    
-}
+var singleNumber = function (nums) {
+  for (i = 0; i < nums.length; i++) {
+    let unique = true;
+    for (j = 0; j < nums.length; j++) {
+      if ((i !== j) & (nums[i] == nums[j])) {
+        unique = false;
+        break;
+      }
+    }
+    if (unique == true) {
+      return i;
+    }
+  }
+};
 
-Unique([1,2,4,6,2,4,6])
+console.log(singleNumber([1, 2, 1, 3, 4, 3, 2]));
