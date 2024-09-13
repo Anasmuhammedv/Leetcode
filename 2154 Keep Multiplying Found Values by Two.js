e7720ab2db644673835c4691b8b79e635eb885b6
Anas <anasanchu3004@gmail.com>
2024-09-13 10:05:58 +0530

    2154 :  Keep Multiplying Found Values by Two

---
+++ 2154 Keep Multiplying Found Values by Two.js
@@ -0,0 +1,15 @@
+var findFinalValue = function(nums, original) {
+  for(i=0;i<nums.length;i++){
+    if(nums[i]==original){
+       original *= 2
+       i = -1 
+    }
+  }
+
+
+return original
+    
+};
+
+
+console.log(findFinalValue([12,5,3,6,1],2));
